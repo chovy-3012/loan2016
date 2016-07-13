fe5eeef78cc5a563e315cadb8f0262a7cd410bd4
zhaowei <virgilchiu@qq.com>
2016-07-13 10:56:30 +0800

    update

--- vergil-loan2016-wechat/src/main/java/io/vergil/loan2016/servlet/WechatServlet.java
+++ vergil-loan2016-wechat/src/main/java/io/vergil/loan2016/servlet/WechatServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * 微信服务端收发消息接口
  * 
@@ -25,8 +26,7 @@ public class WechatServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 
@@ -39,18 +39,18 @@ public class WechatServlet extends HttpServlet {
 		while ((s = br.readLine()) != null) {
 			sb.append(s);
 		}
-		String xml = sb.toString();	//次即为接收到微信端发送过来的xml数据
-
+		String xml = sb.toString(); // 次即为接收到微信端发送过来的xml数据
+		System.out.println("接收数据:" + xml);
 		String result = "";
 		/** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */
 		String echostr = request.getParameter("echostr");
 		if (echostr != null && echostr.length() > 1) {
 			result = echostr;
 		} else {
-			//正常的微信处理流程
+			// 正常的微信处理流程
 			result = new WechatProcess().processWechatMag(xml);
 		}
-
+		System.out.println("发送数据:" + result);
 		try {
 			OutputStream os = response.getOutputStream();
 			os.write(result.getBytes("UTF-8"));
@@ -76,8 +76,7 @@ public class WechatServlet extends HttpServlet {
 	 * @throws IOException
 	 *             if an error occurred
 	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
